3887e55115f4d7e8b4486f613b53a83e360f84e1
kadiri2006 <kadiri2006@gmail.com>
2022-05-12 19:51:05 +0530

    before commit  is fixed by using " useMemo "

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { MemoChild } from "./Child";
 
 export default function Parent({ children }) {
   const [change, setChange] = useState(0);
-  let myNumber={value:"2"}
+  let myNumber = { value: "2" };
+  let memoMyNumber = useMemo(() => myNumber, []);
   console.log("parent componet render");
   return (
     <>
       <h1>parent component</h1>
       <button onClick={() => setChange(change + 1)}>ParentChange</button>
-      
-      <MemoChild num={myNumber}/>
+
+      <MemoChild num={memoMyNumber} />
     </>
   );
 }
