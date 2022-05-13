f8767f7b70381ac8c5651b7985cc0b3eafbc4ca7
kadiri2006 <kadiri2006@gmail.com>
2022-05-13 12:01:20 +0530

    react default optimization

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Child from "./Child";
 import Parent from "./Parent";
 
 export default function App() {
+  const [first, setfirst] = useState(true)
   console.log("render App");
 
-  return <Parent />;
+  return (
+    <>
+      <strong>App</strong>
+      <button onClick={()=>setfirst(!first)}>btn at app state chage</button>
+      <Parent>
+        <Child />
+      </Parent>
+    </>
+  );
 }
--- src/Parent.js
+++ src/Parent.js
@@ -1,18 +1,16 @@
-import React, { useState } from 'react'
-import Child from './Child';
+import React, {useState } from "react";
 
-export default function Parent() {
-    
-    const [first, setfirst] = useState(0);
-    console.log("render parent");
-    
+export default function Parent({ children }) {
+  const [first, setfirst] = useState(0);
+  console.log("render parent");
 
   return (
-      <>
-          <div>Parent</div>
-          <button onClick={()=>setfirst(first+1)}>Parent btn state change</button>
-      <Child/>
-      </>
-      
-  )
+    <>
+      <div>Parent</div>
+      <button onClick={() => setfirst(first + 1)}>
+        Parent btn state change
+      </button>
+      {children}
+    </>
+  );
 }
