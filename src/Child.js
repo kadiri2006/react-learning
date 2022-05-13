7543e89f3885e6729b772cd1992d7fc8d3ddf685
kadiri2006 <kadiri2006@gmail.com>
2022-05-13 10:05:44 +0530

    react memo do't optimize case-1

--- src/Child.js
+++ src/Child.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Child2 from "./Child2";
 
-export default function Child() {
+
+export default function Child({children}) {
   console.log("render child");
   return (
     <>
       <div>Child 1</div>;
-      <Child2 />
+      {children}
     </>
   );
 }
--- src/Parent.js
+++ src/Parent.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import { MemoChild } from './Child';
+import React from "react";
+import { MemoChild } from "./Child";
+import Child2 from "./Child2";
 
-
-export default function Parent({first}) {
-  console.log("render parent",first);
-    return (
-        <>
-    <div>Parent</div>
-        <MemoChild/>
-        </>
-  )
+export default function Parent({ first }) {
+  console.log("render parent", first);
+  return (
+    <>
+      <div>Parent</div>
+      <MemoChild>
+        <Child2 />
+      </MemoChild>
+    </>
+  );
 }
